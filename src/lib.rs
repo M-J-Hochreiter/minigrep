e25f31241ce36a620c25ec6ad09c6db788bf5f4e
M-J-Hochreiter <77507116+M-J-Hochreiter@users.noreply.github.com>
2025-02-25 12:01:34 +0100

    using functional style for search

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::char::ToLowercase;
 use std::error::Error;
 use std::env;
 use std::fs;
@@ -46,26 +47,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
 
-    result
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
